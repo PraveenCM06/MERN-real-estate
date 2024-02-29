e8780fab9cbcc25fcc564932434925c893f40eeb
Praveen <praveencm427@gmail.com>
2024-02-29 14:10:41 +0530

    connected to Database(MONGO_DB)

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { log } from 'console';
 
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to DB");
+}).catch(error=>{
+    console.log(error);
+    console.error("BAD AUTHENTICATION!!");
+})
 const port =3000;
 
 const app = express();
