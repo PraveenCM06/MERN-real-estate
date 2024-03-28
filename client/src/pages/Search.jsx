f159d1a81d969ce5555d2d3cb3659b3f1e3ada3f
Praveen CM <praveencm427@gmail.com>
2024-03-28 15:30:10 +0530

    added show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -13,7 +13,7 @@ function Search() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-
+  const [showMore, setShowMore] = useState(false);
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermfromUrl = urlParams.get("searchTerm");
@@ -43,9 +43,15 @@ function Search() {
 
     const fetchListings = async()=>{
         setLoading(true);
+        setShowMore(false);
         const searchQuery = urlParams.toString();
         const response = await fetch(`/api/listing/get?${searchQuery}`);
         const data = await response.json();
+        if(data.length>8){
+            setShowMore(true);
+        }else{
+            setShowMore(false);
+        }
         setListings(data);
         setLoading(false);
     }
@@ -94,6 +100,19 @@ function Search() {
     navigate(`/search?${searchQuery}`);
   };
 
+  const onShowMoreClick = async ()=>{
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const results= await fetch(`/api/listing/get?${searchQuery}`);
+    const data= await results.json();
+    if(data.length<9){
+        setShowMore(false);
+    }
+    setListings([...listings, ...data]);
+  };
 
   return (
     <div className="flex flex-col md:flex-row p-2 gap-4 ">
@@ -204,7 +223,11 @@ function Search() {
             {!loading && listings && listings.map((listing)=>(
                 <ListingCard key={listing._id} listing={listing}/>
             ))}
+            {showMore && (
+                <button className="text-green-700 hover:underline p-7 w-full text-center" onClick={onShowMoreClick}>Show More..</button>
+            )}
         </div>
+
       </div>
     </div>
   );
